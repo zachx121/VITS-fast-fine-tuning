97747808d19d33bfed2bc393cfd0b4b89893c2c2
zhoutongzt <zhoutongzt@didiglobal.com>
2023-05-31 00:28:44 +0800

    upd | add rm -rf

--- run.sh
+++ run.sh
@@ -4,6 +4,9 @@ conda config --add envs_dirs /nfs/project/opt/miniconda3/envs
 source activate py38_torch2
 
 export LD_LIBRARY_PATH=/nfs/project/opt/miniconda3/envs/py38_torch2/lib/python3.8/site-packages/nvidia/cublas/lib/:$LD_LIBRARY_PATH
+# 清理之前的去噪数据等
+rm -rf denoised_audio/*
+rm -rf segmented_character_voice/audio/*
 # 对所有上传的数据进行自动去背景音&标注, 需要调用Whisper和Demucs，运行时间可能较长。
 # 将所有音频（无论是上传的还是从视频抽取的，必须是.wav格式）去噪
 python scripts/denoise_audio.py
@@ -16,9 +19,11 @@ python scripts/resample.py
 
 # 选择是否加入辅助训练数据：/ Choose whether to add auxiliary data:
 # ADD_AUXILIARY=True
-python preprocess_v2.py --add_auxiliary_data True --languages "CJ"
+#python preprocess_v2.py --add_auxiliary_data True --languages "CJ"
+# 辅助训练数据只用中文的
+python preprocess_v2.py --add_auxiliary_data True --languages "C"
 # ADD_AUXILIARY=False
-# python preprocess_v2.py --languages "CJ"
+#python preprocess_v2.py --languages "CJ"
 
 
 #import os
