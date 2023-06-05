76c98384153db7d283f71947eff543234011553e
zhoutongzt <zhoutongzt@didiglobal.com>
2023-06-05 23:13:13 +0800

    upd | speech2text.py | class style

--- scripts/speech2text.py
+++ scripts/speech2text.py
@@ -8,17 +8,33 @@ import sys
 import timeit
 
 MODEL_TYPE = sys.argv[1] if len(sys.argv) >= 2 else "tiny"
+
+
+class Speech2Text:
+    def __init__(self, model_type, download_root="./whisper_models"):
+        self.model_type = model_type
+        self.download_root = download_root
+        self.model = None
+
+    def init(self):
+        self.model = whisper.load_model(self.model_type, download_root=self.download_root)
+
+    def transcribe(self, audio_file):
+        assert self.model is not None, "self.model is None, should call '.init()' at first"
+        result = self.model.transcribe(audio_file,
+                                       task="transcribe",
+                                       beam_size=5,
+                                       best_of=5,
+                                       word_timestamps=False)
+        return result['text']
+
+
 print(">>> use MODEL_TYPE as '%s'" % MODEL_TYPE)
-model = whisper.load_model(MODEL_TYPE, download_root="./whisper_models")
-options = dict(beam_size=5, best_of=5)
-transcribe_options = dict(task="transcribe", **options)
-res = timeit.timeit(lambda: model.transcribe("./audio_daniel_2021-part0.wav",
-                                             word_timestamps=True, **transcribe_options),
-                    number=10)
-print(">>> timeit: %s" % res)
-result = model.transcribe("./audio_daniel_2021-part0.wav",
-                          word_timestamps=True, **transcribe_options)
-print(result["text"])
+M_stt = Speech2Text(model_type=MODEL_TYPE, download_root="./whisper_models")
+time_res = timeit.timeit(lambda: M_stt.transcribe("./audio_daniel_2021-part0.wav"), number=10)
+print(">>> timeit: %s" % time_res)
+text = M_stt.transcribe("./audio_daniel_2021-part0.wav")
+print(">>> transcribe:\n%s" % text)
 
 # import pickle
 # with open("./result_%s.pkl" % MODEL_TYPE, "wb") as f:
