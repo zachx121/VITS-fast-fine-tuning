e9db56ff10a4cc3171189826329cb2a54d7c9c4a
zhoutongzt <zhoutongzt@didiglobal.com>
2023-06-05 22:36:32 +0800

    upd | sys.argv for speech2

--- scripts/speech2text.py
+++ scripts/speech2text.py
@@ -4,7 +4,10 @@ import whisper
 # tiny,base,small,medium,large-v1,large-v2
 # VITS用的是medium
 import whisper
-MODEL_TYPE = "tiny"
+import sys
+
+MODEL_TYPE = sys.argv[1] if len(sys.argv) >= 2 else "tiny"
+print(">>> use MODEL_TYPE as '%s'" % MODEL_TYPE)
 model = whisper.load_model(MODEL_TYPE, download_root="./whisper_models")
 options = dict(beam_size=5, best_of=5)
 transcribe_options = dict(task="transcribe", **options)
@@ -12,9 +15,9 @@ result = model.transcribe("./audio_daniel_2021-part0.wav",
                           word_timestamps=True, **transcribe_options)
 print(result["text"])
 
-import pickle
-with open("./result_%s.pkl" % MODEL_TYPE, "wb") as f:
-    pickle.dump(result, f)
+# import pickle
+# with open("./result_%s.pkl" % MODEL_TYPE, "wb") as f:
+#     pickle.dump(result, f)
 
 # import json
 # print(json.dumps(result))
