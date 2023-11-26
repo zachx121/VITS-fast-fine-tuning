2c2b3c1c07bdcd7456a4c018d6039e2c6ff53c2a
zach <zachx121@outlook.com>
2023-11-26 19:06:33 +0800

    语音识别增加了补零(需请求时提供采样率和时长

--- server_vits_mp_speech2text.py
+++ server_vits_mp_speech2text.py
@@ -43,15 +43,9 @@ import wave
 DEVICE = "cuda:0" if torch.cuda.is_available() else "cpu"
 logging.info(">>> [DEVICE]: %s" % DEVICE)
 
-# [Params]
-# PORT = int(sys.argv[1]) if len(sys.argv) >= 2 else 8080
-# TTS_MODEL = sys.argv[2] if len(sys.argv) >= 3 else "./vits_models/G_latest_cxm_1st.pth"
-# TTS_MODEL_DIR = sys.argv[2] if len(sys.argv) >= 3 else "./sounda_voice_model_v1"
-# SST_MODEL_DIR = sys.argv[3] if len(sys.argv) >= 4 else "./whisper_models"
 DEBUG = False
 PORT = 6006
 PROCESS_NUM = 2
-TTS_MODEL_DIR = "/root/autodl-fs/vits_models/OUTPUT_MODEL_四郎_daniel_bruce"
 SST_MODEL_DIR = "/root/autodl-fs/whisper"
 WHISPER_MODEL = "large-v2"  # large-v2, tiny, medium
 OUTPUT_DIR = "./output"
@@ -59,7 +53,6 @@ VOICE_SAMPLE_DIR = "./voice_sample"
 os.makedirs(OUTPUT_DIR, exist_ok=True)
 os.makedirs(VOICE_SAMPLE_DIR, exist_ok=True)
 logging.info(">>> [PORT]: %s" % PORT)
-logging.info(">>> [TTS_MODEL_DIR]: %s" % TTS_MODEL_DIR)
 logging.info(">>> [SST_MODEL_DIR]: %s" % SST_MODEL_DIR)
 
 
@@ -112,6 +105,7 @@ def process_queue_speech2text(q_input, q_output, sid_info, lock, _pid_name):
                 lb = len(info["buffer"])
                 volume = audioop.rms(info["buffer"][lb - int(RMS_LAST_TIME * BYTES_PER_SEC):lb], SAMPLE_WIDTH)
                 logging.debug(_pid_name[os.getpid()] + "    最后 %s 秒的音量: %s (buffer_len:+%s=%s text:'%s')" % (RMS_LAST_TIME, volume,len(data['audio']), lb, info["text"][:15]))
+                # todo.转录已经识别到句号也清空
                 if volume <= RMS_HOLDER:
                     logging.debug(_pid_name[os.getpid()] + "    最后 %s 秒的音量小于阈值, 清空buffer" % RMS_LAST_TIME)
                     logging.debug(_pid_name[os.getpid()] + "    清空前再转录一次文本")
@@ -123,10 +117,23 @@ def process_queue_speech2text(q_input, q_output, sid_info, lock, _pid_name):
                                                    return_details=DEBUG,prob_holder=NS_PROB_HOLDER)
                     eos_tag = True
                     info["buffer"] = b""
+                # 持续超过90s的buffer也直接转录后清空
+                if lb//int(RMS_LAST_TIME * BYTES_PER_SEC) > 90:
+                    logging.debug(_pid_name[os.getpid()] + "    总长度'%s'超过90s, 清空buffer" % lb//int(RMS_LAST_TIME * BYTES_PER_SEC))
+                    logging.debug(_pid_name[os.getpid()] + "    清空前再转录一次文本")
+                    text = model.transcribe_buffer(info["buffer"],
+                                                   sr_inp=SAMPLE_RATE,
+                                                   channels_inp=CHANNELS,
+                                                   language=lang,
+                                                   fp16=False,
+                                                   return_details=DEBUG,prob_holder=NS_PROB_HOLDER)
+                    eos_tag = True
+                    info["buffer"] = b""
+
 
         # 尝试在锁外发送消息（主进程有新的客户端连接时，需要拿锁去更新SID_INFO，如果此时子进程占着锁发送无超时的curl，等待主进程返回结果，那就形成死锁了）
         if eos_tag:
-            logging.debug(_pid_name[os.getpid()] + "    识别到结束，发送最终文本 '%s'" % text[:20])
+            logging.debug(_pid_name[os.getpid()] + "    识别到结束，发送最终文本(预览前20字) '%s'" % text[:20])
             rsp = json.dumps({"text": text, "mid": "0", "trace_id": data.get("trace_id","")})
             # socketio.emit("speech2text_rsp", rsp, to=sid, namespace=NAME_SPACE)
             q_output.put((rsp, sid))
@@ -163,53 +170,6 @@ def process_queue_speech2text(q_input, q_output, sid_info, lock, _pid_name):
                 info = sid_info[sid]
                 info["text"] = text
 
-# 子线程用vits进行声音合成（文本转语音）
-def process_queue_text2speech(q_input, q_output):
-    logging.info("process_queue_text2speech start.")
-    logging.info(">>> Construct&Init Model (device is '%s')" % DEVICE)
-    M_tts = Text2Speech_vits(model_dir=os.path.join(TTS_MODEL_DIR,"G_latest.pth"),
-                             config_fp=os.path.join(TTS_MODEL_DIR,"config.json"))
-    M_tts.init()
-    logging.info(">>> Construct&Init Model done.")
-    logging.info(">>> Speakers: %s" % ",".join(M_tts.hparams['speakers'].keys()))
-
-    while True:
-        # 从队列中获取数据
-        data, t_str, sid = q_input.get()
-        ts_data = int(datetime.strptime(t_str, "%Y-%m-%d %H:%M:%S").timestamp())
-        if data is None:
-            logging.info("data is None, break now. %s" % t_str)
-            break
-        t_begin = time.time()
-        logging.debug("  Process of sid-%s-%s start." % (sid, t_str))
-
-        if data["speaker"] in M_tts.hparams['speakers'].keys():
-            sr, wav = M_tts.tts_fn(text=data["text"],
-                                   speaker=data["speaker"],
-                                   language="auto",  # 用Mix的话就相当于直接读字母发音了
-                                   text_cleaners=M_tts.hparams['data']['text_cleaners'],
-                                   speed=1)
-            # 默认得到的是np.float32，32位深的结果，这里改成16位深
-            wav_scaled = np.clip(wav, -1.0, 1.0)
-            wav_16bit = (wav_scaled * 32767).astype(np.int16)
-            rsp = {"trace_id": data.get("trace_id",""),
-                   "audio_buffer": base64.b64encode(wav_16bit.tobytes()).decode(),
-                   "sr": str(sr),
-                   "status": "0",
-                   "msg": "success."}
-            rsp = json.dumps(rsp)
-            q_output.put((rsp, sid))
-            _ = os.system("curl -m 5 127.0.0.1:%s/exec_emit_text2speech" % PORT)
-        else:
-            logging.error("not found speaker: '%s'" % data["speaker"])
-            rsp = {"trace_id": data.get("trace_id",""),
-                   "audio_buffer": "",
-                   "sr": "",
-                   "status": "1",
-                   "msg": "fail. not found speaker '%s'" % data['speaker']}
-            rsp = json.dumps(rsp)
-            q_output.put((rsp, sid))
-            _ = os.system("curl -m 5 127.0.0.1:%s/exec_emit_text2speech" % PORT)
 
 # Flask Service init
 def create_app():
@@ -226,26 +186,6 @@ def create_app():
     # curl 127.0.0.1:8080/debug
     @app.route("/debug")
     def debug_func():
-        logging.info("will send to clinet.")
-        socketio.emit("get_server_info", {"data": "this is a test message"}, namespace=NAME_SPACE)
-        socketio.emit("audio_rsp", {"text": "manually debug send."}, namespace=NAME_SPACE)
-        if DEBUG:
-            for sid, audio in AUDIO_RECORD.items():
-                wf_sid = wave.open(os.path.join(OUTPUT_DIR, 'audio_record_%s.wav' % sid), 'wb')
-                wf_sid.setnchannels(CHANNELS)
-                wf_sid.setsampwidth(SAMPLE_WIDTH)
-                wf_sid.setframerate(SAMPLE_RATE)
-                wf_sid.writeframes(audio)
-
-        with LOCK:
-            with open(os.path.join(OUTPUT_DIR, "debug.pkl"), "wb") as fwb:
-                pickle.dump(dict(SID_INFO), fwb)
-            for sid in SID_INFO:
-                wf_sid = wave.open(os.path.join(OUTPUT_DIR, 'sid_%s.wav' % sid), 'wb')
-                wf_sid.setnchannels(CHANNELS)
-                wf_sid.setsampwidth(SAMPLE_WIDTH)
-                wf_sid.setframerate(SAMPLE_RATE)
-                wf_sid.writeframes(SID_INFO[sid]["buffer"])
         return "message send!\n"
 
     # 针对NAME_SPACE域里的socket连接的「connect」事件
@@ -275,13 +215,27 @@ def create_app():
     def speech2text(data):
         data = json.loads(data)
         data["audio"] = base64.b64decode(data["audio"])
+        logging.info("收到的buffer长度: %s" % len(data["audio"]))
+        # 如果请求里提供了必要信息，可以尝试末尾补零
+        # - 正常情况下时长、采样率等数据示例 elapse:1.0, chunk_size: 22050, buffer_len: 44100, buffer2arr_shape: (22050,)
+        # - 此处要求音频位宽必须是2（后续whisper处理的时候写死了buffer转arr用的int16即2个字节，约定好的暂时不改了）
+        sr = data.get('sample_rate', None)
+        elapse = data.get('elapse', None)
+        sw = data.get('sample_width', None)
+        if all([i is not None for i in [sr, elapse, sw]]) and sw == 2:
+            chunk_size = elapse * sr
+            if len(data["audio"])//sw < chunk_size:
+                logging.debug("发现长度不够的buffer，末尾补零")
+                pad_zeros = np.zeros(int(chunk_size-len(data["audio"])//sw), dtype=np.int16)
+                pad_res = np.hstack([np.frombuffer(data["audio"], dtype=np.int16), pad_zeros])
+                data["audio"] = pad_res.tobytes()
+                logging.info("补零后的buffer长度: %s" % len(data["audio"]))
+
         sid = request.sid
         # ts = int(time.time())
         ts = int(data['ts'])
         t_str = datetime.fromtimestamp(ts).strftime("%Y-%m-%d %H:%M:%S")
         logging.debug(f"{NAME_SPACE}/speech2text received an input.(%s %s)" % (ts, t_str))
-        if DEBUG:
-            AUDIO_RECORD.update({sid: AUDIO_RECORD.get(sid, b"") + data["audio"]})
 
         # 将数据添加到队列中
         Q_speech2text.put((data, t_str, sid))
@@ -300,47 +254,6 @@ def create_app():
                 break
         return ""
 
-    @socketio.on("text2speech", namespace=NAME_SPACE)
-    def text2speech(data):
-        # 放到子线程里做
-        data = json.loads(data)
-        ts = int(time.time())
-        t_str = datetime.fromtimestamp(ts).strftime("%Y-%m-%d %H:%M:%S")
-        logging.info(f"{NAME_SPACE}/text2speech received an input.(%s %s)" % (ts, t_str))
-
-        # 将数据添加到队列中
-        Q_text2speech.put((data, t_str, request.sid))
-
-
-    @app.route("/exec_emit_text2speech")
-    def exec_emit_text2speech():
-        # logging.debug("触发了向客户端发送消息（持续从队列中加载直到没有可加载的）")
-        while True:
-            try:
-                rsp, sid = Q_text2speech_rsp.get_nowait()
-                # logging.debug("    队列拿到结果 —— rsp:'%s...'" % str(rsp[:15]))
-                socketio.emit("text2speech_rsp", rsp, to=sid, namespace=NAME_SPACE)
-            except Empty:
-                # logging.debug("    队列为空，退出")
-                break
-        return ""
-
-    @socketio.on("upload_speaker", namespace=NAME_SPACE)
-    def upload_speaker(data):
-        print("upload_speaker received ...")
-        data = json.loads(data)
-        data["audio"] = base64.b64decode(data["audio"])
-        ts = int(time.time())
-        t_str = datetime.fromtimestamp(ts).strftime("%Y-%m-%d %H:%M:%S")
-        logging.info(f"{NAME_SPACE} upload_speaker received an input.(%s %s)" % (ts, t_str))
-
-        wf_sid = wave.open(os.path.join(VOICE_SAMPLE_DIR, data["speaker"]+"_"+data["language"]+".wav"), 'wb')
-        wf_sid.setnchannels(CHANNELS)
-        wf_sid.setsampwidth(SAMPLE_WIDTH)
-        wf_sid.setframerate(SAMPLE_RATE)
-        wf_sid.writeframes(data['audio'])
-        socketio.emit("upload_speaker_rsp", json.dumps({"status": "0"}), to=request.sid, namespace=NAME_SPACE)
-
     return app, socketio
 
 
@@ -349,8 +262,6 @@ if __name__ == '__main__':
     manager = mp.Manager()
     Q_speech2text = manager.Queue()
     Q_speech2text_rsp = manager.Queue()
-    Q_text2speech = manager.Queue()
-    Q_text2speech_rsp = manager.Queue()
     SID_INFO = manager.dict()
     LOCK = mp.Lock()
     _PID_NAME = manager.dict()
@@ -363,13 +274,6 @@ if __name__ == '__main__':
         processes.append(p1)
         _PID_NAME.update({p1.pid: "子进程%s" % idx})
         logging.info("    speech2text子进程启动 (%s)" % p1.pid)
-
-        p2 = mp.Process(target=process_queue_text2speech,
-                        args=(Q_text2speech, Q_text2speech_rsp))
-        p2.start()
-        processes.append(p2)
-        _PID_NAME.update({p2.pid: "子进程%s" % idx})
-        logging.info("    text2speech子进程启动 (%s)" % p2.pid)
         
     logging.info(">>> MultiProcess ready. all pid as follow:")
     logging.info(_PID_NAME)
