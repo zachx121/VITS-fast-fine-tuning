d2ea031c130a0a35d1524afeb8d05b98b5e1174f
Zachary4biz <zachary4biz@yeah.net>
2023-11-27 03:00:31 +0800

    API部署搞定

--- autodl_deploy.py
+++ autodl_deploy.py
@@ -2,11 +2,24 @@
 import json
 import logging
 import os
-
+import sys
 import requests
 import time
 
-mode = "speech2text"  # speech2text, text2speech
+assert len(sys.argv) >=3, "第一个参数是nums控制启动几个服务，第二个参数是mode控制speech2text/text2speech"
+nums = sys.argv[1]  # e.g. 5表示启动5个docker
+mode = sys.argv[2]  # speech2text, text2speech
+# large模型在3090-24G上只能启动2个(21G); medium在v100-32G能启动4个(28G); base在v100-32G能启动17个(28G)
+# base~1.7G; medium~7G; large~10.5G
+# type_speech2text, nums_speech2text = "large-v2", 2
+type_speech2text, nums_speech2text = "medium", 3
+# type_speech2text, nums_speech2text = "base", 12
+nums_text2speech = 12
+cmd_dict = {
+    "speech2text": f"cd Serving_VITS-fast-fine-tuning/ && python server_vits_mp_speech2text.py {nums_speech2text} {type_speech2text}",
+    "text2speech": f"cd Serving_VITS-fast-fine-tuning/ && python server_vits_mp_text2speech.py {nums_text2speech}"
+}
+
 headers = {
     "Authorization": "eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOjIxMjM5MiwidXVpZCI6ImVmY2MyMWUwLWUwYzYtNDY4Ny05N2MwLTg4OTljYTQzYjRlZiIsImlzX2FkbWluIjpmYWxzZSwiYmFja3N0YWdlX3JvbGUiOiIiLCJpc19zdXBlcl9hZG1pbiI6ZmFsc2UsInN1Yl9uYW1lIjoiIiwidGVuYW50IjoiYXV0b2RsIiwidXBrIjoiIn0.RvIaNq0u2f0X-_gmhJgutOq4xY01XLAeXyhRJyrDH-1bUQwtLyIrgsvxvtw0yTAbnLE4oRk-jju1RkSIMmYsTg",
     "Content-Type": "application/json"
@@ -22,7 +35,7 @@ url = host+"/api/v1/dev/deployment"
 body = {
     "name": "api自动创建",
     "deployment_type": "ReplicaSet",
-    "replica_num": 2,
+    "replica_num": nums,
     "container_template": {
         "region_sign": "beijingDC1",  # 容器可调度的地区
         "gpu_name_set": ["RTX 3090"],  # 可调度的GPU型号
@@ -32,7 +45,7 @@ body = {
         "cpu_num_to": 100,
         "memory_size_from": 32,  # 可调度的容器内存大小范围。单位：GB
         "memory_size_to": 256,
-        "cmd": "cd Serving_VITS-fast-fine-tuning/ && python server_vits_mp_speech2text.py > nohup.out 2>&1 && sleep infinity ",  # 启动容器命令
+        "cmd": "%s > nohup.out 2>&1 && sleep infinity " % cmd_dict[mode],  # 启动容器命令
         "price_from": int(0.1*1000),  # 可调度的价格范围。单位：元 * 1000，如0.1元填写100
         "price_to": int(2*1000),
         "image_uuid": "image-8e6a5a391c",
@@ -55,8 +68,7 @@ for i in range(10):
         logging.info("部署还未完成 ...")
     time.sleep(10)  # 10s
 
-
-time.sleep(30)
+time.sleep(10)
 # 拿到这次部署的所有container的service_url测试服务
 body = {
     "deployment_uuid": deploy_uuid,
@@ -68,5 +80,5 @@ container_info = json.loads(response.content.decode())
 urls = [i["info"]["service_url"] for i in container_info["data"]["list"]]
 print(urls)
 
-for host in urls:
-    os.system("python debug.py speech2text %s" % host)
+# for host in urls:
+#     os.system("python debug.py %s %s" % (mode, host))
